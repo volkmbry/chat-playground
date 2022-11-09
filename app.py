1f4c7175ff2a80b0d2873d0d1668bddc5a15088a
Adrian Moser <mosa@zhaw.ch>
2022-11-09 10:14:50 +0100

    update

--- app.py
+++ app.py
@@ -13,31 +13,43 @@ st.set_page_config(page_title="Chatbot")
 st.write("Welcome to the DialoGPT-medium chatbot example.")
 tokenizer, model = load_data()
 
-text_input_container = st.empty()
-input = text_input_container.text_input(key="user", label="Enter your question:")
-
-if 'count' not in st.session_state or st.session_state.count == 6:
-    st.session_state.count = 0
+# Init Session State
+if 'step' not in st.session_state:
+    st.session_state.step = 0
     st.session_state.chat_history_ids = None
-    st.session_state.old_response = ''
+    st.session_state.history = []
 else:
-    st.session_state.count += 1
+    st.session_state.step+=1
 
-new_user_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt')
 
-bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids],
-                          dim=-1) if st.session_state.count > 1 else new_user_input_ids
+form = st.form("form", True)
+input = form.text_input(key="user", label="Enter your question:")
+form.form_submit_button("Submit")
 
-st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
+if input:
+    # encode the new user input, add the eos_token and return a tensor in Pytorch
+    new_user_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt')
 
-response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    # append the new user input tokens to the chat history
+    bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1) if st.session_state.step > 1 else new_user_input_ids
 
-if st.session_state.old_response == response:
-    bot_input_ids = new_user_input_ids
+    # generated a response while limiting the total chat history to 1000 tokens, 
+    st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-    st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)   
+    # pretty print last ouput tokens from bot
     response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    st.session_state.history.append(input)
+    st.session_state.history.append(response)
 
+if st.button('Reset'):
+    st.session_state.step = 0
+    st.session_state.chat_history_ids = None
+    st.session_state.history = []
 
-st.write(f"Bot:  \n{response}")
-st.session_state.old_response = response
+for x in range(len(st.session_state.history)):
+    person = "**User**" if x%2==0 else "**Bot**"
+    st.write(person, st.session_state.history[x])
+    
+st.write("Step: ", st.session_state.step)
+st.write(st.session_state.chat_history_ids)
+
